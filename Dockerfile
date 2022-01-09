0a4ab525fdb845601bf071c336ce15a05ea2d085
sg20180546 <sg20180546@gmail.com>
2022-01-09 18:54:43 +0900

    learndocker1

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM node:10
 
 # RUN command
-# WORKDIR /usr/src/app
+WORKDIR /usr/src/app
 
-COPY ./ ./
+COPY package*.json ./
 
 RUN npm install
+
+COPY ./ ./
 
 CMD ["node","server.js"]
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import express from 'express';
+// import express from 'express';
 
+const express=require('express');
 const app=express();
 const PORT=8080;
 const HOST='0.0.0.0';
 
 app.get('/',(req,res)=>{
-    res.send('hello world');
+    res.send('hihihiddfdahi');
 })
 
 app.listen(PORT,HOST);
