5d64d9305a59ce16d0f9818f317e1722dba92009
sg20180546 <sg20180546@gmail.com>
2022-01-11 15:05:26 +0900

    connect to redis container

--- docker-compose-app/server.js
+++ docker-compose-app/server.js
@@ -1,27 +1,18 @@
-const express = require("express");
-const redis = require("redis");
-//레디스 클라이언트 생성 
-const client = redis.createClient({
+const express=require('express');
+const redis=require('redis');
+
+const client=redis.createClient({
     host: "redis-server",
     port: 6379
 })
-
-const app = express();
-
-//숫자는 0 부터 시작합니다.
-client.set("number", 0);
-
-app.get('/', (req, res) => {
-    client.get("number", (err, number) => {
-        //현재 숫자를 가져온 후에 1씩 올려줍니다.
-        // res.send("숫자가 1씩 올라갑니다. 숫자: " + number)
-        client.set("number", parseInt(number) + 1)
-    })
-    res.send("hello");
-})
-
-
+const app=express();
 app.listen(8080);
-console.log('Server is running');
 
+client.set("number",0);
 
+app.get('/',(req,res)=>{
+    client.get("number",(err,number)=>{
+        client.set("number",parseInt(number)+1);
+        res.send("안녕하세여! "+number)
+    })
+})
