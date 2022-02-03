a453ae5076f6b718d9c5cf6c52f2d7bba5de73ed
sg20180546 <sg20180546@gmail.com>
2022-02-03 13:53:51 +0900

    try2

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ const db=require('./db');
 const app=express();
 
 app.use(bodyParser.json());
-db.pool.query(`CREATE TABLE lists(
-    id INTEGER AUTO_INCREMENT,
-    value TEXT,
-    PRIMARY KEY (id)
-);`,(err,result,fields)=>{
-})
+// db.pool.query(`CREATE TABLE lists(
+//     id INTEGER AUTO_INCREMENT,
+//     value TEXT,
+//     PRIMARY KEY (id)
+// );`,(err,result,fields)=>{
+// })
 app.get('/api/hello',(req,res)=>{
     // console.log("./ hello")
     res.status(200).send({
@@ -20,31 +20,68 @@ app.get('/api/hello',(req,res)=>{
     })
 })
 
-app.get('/api/values',(req,res)=>{
+async function save_data(req,res) {
+    try{
+        const connection= await db.pool.getConnection();
+        await connection.query('USE myapp;')
+        connection.query(`INSERT INTO lists (value) VALUE("${req.body.value}")`,(err,result,field)=>{
+            
+            if(err) res.status(510).send("db error");
+            else res.json({status:"success",value:req.body.value});
+        })
+        }finally{
+            await connection.release();
+        }
+}
+
+const get_data =async(req,res)=>{
+    try{
+
+    
+        const connection=await db.pool.getConnection();
+        await connection.query('USE myapp;')
+        connection.query('SELECT * FROM lists;',(err,result,field)=>{
+            res.json(result);
+            if(err) res.status(510).send("db error");
+        })
+        }finally{
+            await connection.release();
+        }
+}
+
+// app.get('/api/values',(req,res)=>{
+
+
     // console.log("hello1");
+    // db.pool.query(`USE myapp;`)
+    // db.pool.query('USE myapp;',
+    // (err,results,fields)=>{
+    //     db.pool.query('SELECT * FROM lists;',(err,result,fields)=>{
+    //         res.json(results);
+    //     })
+    //     if(err){
+    //         return res.status(500).send("backend working1 but db");
+    //     } 
+    //     else return res.json(results);
+    // })
+// })
+app.get('/api/values',get_data);
+app.post('/api/value',save_data);
 
-    db.pool.query('SELECT * FROM lists;',
-    (err,results,fields)=>{
-        if(err){
-            db.pool.query(`USE myapp;`)
-            return res.status(500).send("backend working1 but db");
-        } 
-        else return res.json(results);
-    })
-})
-
-app.post('/api/value',(req,res,next)=>{
-    db.pool.query(`INSERT INTO lists (value) VALUE("${req.body.value}")`,
-    (err,results,field)=>{
-        // host:process.env.MYSQL_HOST,
-        // user:process.env.MYSQL_USER,
-        // password:process.env.MYSQL_PASSWORD,
-        // database:process.env.MYSQL_DATABASE,
-        // port:process.env.MYSQL_PORT
-        if(err) return res.status(501).send("backend working2 but db");
-        else return res.json({success:true,value:req.body.value});
-    })
-})
+// app.post('/api/value',(req,res,next)=>{
+    // save_data(req,res);
+    // db.pool.query(`USE myapp;`);
+    // db.pool.query(`INSERT INTO lists (value) VALUE("${req.body.value}")`,
+    // (err,results,field)=>{
+    //     // host:process.env.MYSQL_HOST,
+    //     // user:process.env.MYSQL_USER,
+    //     // password:process.env.MYSQL_PASSWORD,
+    //     // database:process.env.MYSQL_DATABASE,
+    //     // port:process.env.MYSQL_PORT
+    //     if(err) return res.status(501).send("backend working2 but db");
+    //     else return res.json({success:true,value:req.body.value});
+    // })
+// })
 
 
 
